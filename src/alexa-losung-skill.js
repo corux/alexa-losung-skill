034e4fc7323b50370546287ff5f173ce3b8ee01a
Tobias Wallura <tobias@wallura.org>
2017-03-28 21:37:55 +0200

    extract logic into separate file

--- src/alexa-losung-skill.js
+++ src/alexa-losung-skill.js
@@ -1,47 +1,13 @@
 import { Skill, Launch, Intent, SessionEnded } from 'alexa-annotations';
 import { say, ask } from 'alexa-response';
 import ssml from 'alexa-ssml-jsx';
+import Losungen from './losungen';
 
 @Skill
 export default class AlexaLosungSkill {
 
-  _getLosung(date) {
-    const data = require(`../assets/${date.getFullYear()}.xml`);
-    if (data && data.FreeXml) {
-      const month = ('0' + (date.getMonth() + 1)).slice(-2);
-      const day = ('0' + date.getDate()).slice(-2);
-      const dateString = `${date.getFullYear()}-${month}-${day}T00:00:00`;
-      return data.FreeXml.Losungen.find(n => n.Datum === dateString);
-    }
-
-    throw new Error('Failed to load data');
-  }
-
-  _includeVerse(text) {
-    // replace Psalm 136,3.4 with Psalm 136,3-4
-    const verse = text.substring(text.lastIndexOf(',') + 1);
-    text = text.replace(verse, verse.replace('.', '-'));
-
-    // replace Psalm 136,3-4 with Psalm 136 Vers 3-4
-    return text.replace(/,([^,]*)$/, ' Vers ' + '$1');
-  }
-
   _createResponse(date) {
-    try {
-      const losung = this._getLosung(date);
-      const isToday = date.toDateString() === new Date().toDateString();
-      const spokenDate = isToday ? 'von heute' :
-        `vom ${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
-      return say(<speak>
-        <p>Die Losung {spokenDate} steht in {this._includeVerse(losung.Losungsvers) }</p>
-        <p>{losung.Losungstext}</p>
-        <p>Der Lehrtext steht in {this._includeVerse(losung.Lehrtextvers) }</p>
-        <p>{losung.Lehrtext}</p>
-      </speak>);
-    } catch (e) {
-      const dateString = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
-      return say(`Ich konnte keine Losung für den ${dateString} finden.`);
-    }
+    return say(new Losungen().getText(date));
   }
 
   @Launch
