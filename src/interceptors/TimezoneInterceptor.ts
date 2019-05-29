c84096402950f6c8c7dc52214f4ec09b12f4725b
Tobias Wallura <tobias@wallura.org>
2019-05-30 01:16:35 +0200

    introduced cache for timezone retrieval

--- src/interceptors/TimezoneInterceptor.ts
+++ src/interceptors/TimezoneInterceptor.ts
@@ -3,6 +3,9 @@ import { Settings } from "luxon";
 
 /** Configures Luxon to use the devices timezone instead of UTC. */
 export class TimezoneInterceptor implements RequestInterceptor {
+
+  private static cache: { [deviceId: string]: string } = {};
+
   public async process(handlerInput: HandlerInput) {
     const deviceId = handlerInput.requestEnvelope.context.System.device.deviceId;
 
@@ -11,7 +14,12 @@ export class TimezoneInterceptor implements RequestInterceptor {
     }
 
     try {
-      const timezone = await handlerInput.serviceClientFactory.getUpsServiceClient().getSystemTimeZone(deviceId);
+      if (!TimezoneInterceptor.cache[deviceId]) {
+        const upsServiceClient = handlerInput.serviceClientFactory.getUpsServiceClient();
+        TimezoneInterceptor.cache[deviceId] = await upsServiceClient.getSystemTimeZone(deviceId);
+      }
+
+      const timezone = TimezoneInterceptor.cache[deviceId];
       Settings.defaultZoneName = timezone;
       console.log("Configured timezone for request:", {
         deviceId,
